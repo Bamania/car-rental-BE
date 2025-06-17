cb2391571b785fc46ae45647dd0e8ab4c2dd2afd
Baman Irani <amany29074@gmail.com>
2025-06-17 20:43:50 +0530

    allowed origins

--- index.ts
+++ index.ts
@@ -11,8 +11,8 @@ const app = express();
 const Port = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
-  credentials: true
+  origin: true, // Allow all origins for now
+    credentials: true
 }));
 
 app.use(cookieParser());
